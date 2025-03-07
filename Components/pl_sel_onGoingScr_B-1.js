de6e951e582e24406b76f8f9b4216e679ff4f1a9
navindla <140536677+navindla@users.noreply.github.com>
2025-03-06 23:54:49 -0500

    Update pl_sel_onGoingScr_B-1.js
    aegbda

--- Components/pl_sel_onGoingScr_B-1.js
+++ Components/pl_sel_onGoingScr_B-1.js
@@ -1,4 +1,5 @@
 // here is the build-1 + pl sel ..has bugs: 0-0 no freeze n opp loadr
+// 2nd screen: oppLoader also code in this file exists
 
 
 
@@ -1300,3 +1301,333 @@ const BothTeamOpponentSelection = ({
 export default WithProgress(
   inject("UserStore")(observer(BothTeamOpponentSelection))
 );
+
+
+
+
+
+
+
+
+
+/////////////////////////////////////////////// OPP LOADER ////////////////////////////////////////////////////////////////////////////////////////
+
+
+// has bugs wrong color indication.
+
+// Venky
+
+
+import React, {useEffect, useRef, useState} from 'react';
+import {Image, Text, TouchableOpacity, View, Animated, Easing, ActivityIndicator} from 'react-native';
+import {BorderLine2, CorrectIcon, PlusIcon, TickIcon} from '../../../assets';
+import Modal from 'react-native-modal';
+// import { styles } from '../draft/Draft.styles';
+import { styles } from './BothTeamSelection.styles';
+import { ScrollView } from 'react-native-gesture-handler';
+import { FlatList } from 'react-native';
+import { graybg } from '../../../style';
+
+
+
+
+
+const LoadingPopup = ({global_6players, isVisible, closePopup, navigation, teamA, teamB, teamABench, teamBBench,isOpponent, userInfo, userStoreGdetails}) => {
+    let rotateValueHolder = new Animated.Value(0);
+  const startImageRotateFunction = () => {
+    rotateValueHolder.setValue(0);
+    Animated.timing(rotateValueHolder, {
+      toValue: 1,
+      duration: 1000,
+      easing: Easing.linear,
+      useNativeDriver: true,
+    }).start(() => startImageRotateFunction());
+  };
+
+
+
+
+
+  const renderTeamAItem = ({ item, index }) => {
+
+       const getBorderColor = (item, global_6players, userInfo) => {
+         const matchingPlayer = global_6players.find(
+           (player) => player.playerId === item.playerId
+         );
+         if (matchingPlayer) {
+           return matchingPlayer.userId === userStoreGdetails.userOne
+             ? //matchingPlayer.userId === userInfo?.userId
+             styles.borderTopOrangeWidth
+              // styles.borderTopBlueWidth
+             : styles.borderTopBlueWidth
+         }
+         return styles.defaultBorderStyle; // Optional fallback style
+       };
+   
+
+       //// below code works usr1B////
+       const getTickBgColor = (item, global_6players, userInfo) => {
+         const matchingPlayer = global_6players.find(
+           (player) => player.playerId === item.playerId
+         );
+         if (matchingPlayer) {
+           return matchingPlayer.userId === userStoreGdetails.userOne
+             ? //matchingPlayer.userId === userInfo?.userId
+               "#246afe"
+             : "orange";
+         }
+         return styles.defaultBorderStyle; // Optional fallback style
+       };
+    
+
+
+    // Determine if the player is already selected or disabled
+    const isPlayerDisabled = global_6players.some(
+      (player) => player.playerId === item.playerId
+    );
+
+    return (
+      <TouchableOpacity
+      disabled={true}
+        style={[
+          styles.tmplyrlft,
+          getBorderColor(item, global_6players, userInfo),
+        ]}
+      >
+        {/* Player Left Section */}
+        <View style={styles.plyrlft}>
+          <View style={[styles.plyrprfl, styles.plyrlftprfl]}>
+            <Image
+              style={styles.teamsImageStyle}
+              source={
+                item.imageId
+                  ? {
+                      uri: `https://cdn.sportmonks.com/images/cricket/players/${item.imageId}`,
+                    }
+                  : require("../../../assets/images/dummy.png")
+              }
+              onError={() =>
+                console.error(`Failed to load image for ${item.name}`)
+              }
+            />
+          </View>
+          <View style={{ marginTop: 5 }}>
+            <Text style={[styles.ortxt, styles.plyrtm, styles.plyrsd]}>
+              {item.teamName}
+            </Text>
+          </View>
+        </View>
+
+        {/* Dotted Line Section */}
+        <View style={[styles.dotp, styles.dotp2]}>
+          <Image style={styles.dotimg} source={BorderLine2} />
+        </View>
+
+        {/* Player Info Section */}
+        <View>
+          <Text
+            style={[
+              styles.teamnm,
+              styles.wnrtxt,
+              styles.tmname,
+              styles.plyrname,
+            ]}
+          >
+            {item.name}
+          </Text>
+          <Text style={[styles.teamnm, styles.plyrruns]}>Avg: {item.avg}</Text>
+          <Text style={[styles.ortxt, styles.plyrruns]}>HS: {item.hs}</Text>
+        </View>
+
+        {/* Player Action Button */}
+        <TouchableOpacity
+          style={[
+            styles.adplyr,
+            {
+              backgroundColor: isPlayerDisabled ?  getTickBgColor(item, global_6players, userInfo) : "gray",
+            },
+          ]}
+          disabled={true}
+
+        >
+          {isPlayerDisabled ? (
+            <Image style={{ width: 8, height: 8 }} source={TickIcon} />
+          ) : (
+            <Image style={styles.adplusimg} source={PlusIcon} />
+          )}
+        </TouchableOpacity>
+      </TouchableOpacity>
+    );
+  };
+
+
+
+  const renderTeamBItem = ({ item, index }) => {
+
+    const getBorderColor = (item, global_6players, userInfo) => {
+      const matchingPlayer = global_6players.find(
+        (player) => player.playerId === item.playerId
+      );
+      if (matchingPlayer) {
+        return matchingPlayer.userId === userStoreGdetails.userOne
+          ? //matchingPlayer.userId === userInfo?.userId
+          styles.borderTopOrangeWidth
+           // styles.borderTopBlueWidth
+          : styles.borderTopBlueWidth
+      }
+      return styles.defaultBorderStyle; // Optional fallback style
+    };
+    
+        const getTickBgColor = (item, global_6players, userInfo) => {
+          const matchingPlayer = global_6players.find(
+            (player) => player.playerId === item.playerId
+          );
+          if (matchingPlayer) {
+            return matchingPlayer.userId === userStoreGdetails.userOne
+              ? "#246afe"
+              : "orange";
+          }
+          return styles.defaultBorderStyle; // Optional fallback style
+        };
+
+    // Determine if the player is already selected or disabled
+    const isPlayerDisabled = global_6players.some(
+      (player) => player.playerId === item.playerId
+    );
+
+    return (
+      <View
+        style={[
+          styles.tmplyrlft,
+          getBorderColor(item, global_6players, userInfo),
+        ]}
+      >
+        {/* Player Left Section */}
+        <View style={styles.plyrlft}>
+          <View style={[styles.plyrprfl, styles.plyrlftprfl]}>
+            <Image
+              style={styles.teamsImageStyle}
+              source={
+                item.imageId
+                  ? {
+                      uri: `https://cdn.sportmonks.com/images/cricket/players/${item.imageId}`,
+                    }
+                  : require("../../../assets/images/dummy.png")
+              }
+              onError={() =>
+                console.error(`Failed to load image for ${item.name}`)
+              }
+            />
+          </View>
+          <View style={{ marginTop: 5 }}>
+            <Text style={[styles.ortxt, styles.plyrtm, styles.plyrsd]}>
+              {item.teamName}
+            </Text>
+          </View>
+        </View>
+
+        {/* Dotted Line Section */}
+        <View style={[styles.dotp, styles.dotp2]}>
+          <Image style={styles.dotimg} source={BorderLine2} />
+        </View>
+
+        {/* Player Info Section */}
+        <View>
+          <Text
+            style={[
+              styles.teamnm,
+              styles.wnrtxt,
+              styles.tmname,
+              styles.plyrname,
+            ]}
+          >
+            {item.name}
+          </Text>
+          <Text style={[styles.teamnm, styles.plyrruns]}>Avg: {item.avg}</Text>
+          <Text style={[styles.ortxt, styles.plyrruns]}>HS: {item.hs}</Text>
+        </View>
+
+        {/* Player Action Button */}
+        <TouchableOpacity
+          style={[
+            styles.adplyr,
+            {
+              backgroundColor: isPlayerDisabled ?  getTickBgColor(item, global_6players, userInfo) : "gray",
+            },
+          ]}
+         
+        >
+          { isPlayerDisabled ? (
+            <Image style={{ width: 8, height: 8 }} source={TickIcon} />
+          ) : (
+            <Image style={styles.adplusimg} source={PlusIcon} />
+          )}
+        </TouchableOpacity>
+      </View>
+    );
+  };
+
+
+
+  return (
+      <Modal isVisible={isVisible} style={{marginTop: 100}}>
+        <View style={{paddingHorizontal: 0,paddingVertical: 0,marginBottom: 30,width:'100%'}}>
+          <ScrollView style={[styles.scrollView]}>
+          <Text style={[styles.headingText, styles.ennum,{textAlign : 'center',fontSize:14}]}>Please wait while your opponent selects a player</Text>
+          <View style={[styles.playerContainer,{marginTop : 10}]}>
+              <View style={styles.playerContentBox}>
+                  <FlatList
+                      data={teamA}
+                      renderItem={renderTeamAItem}
+                    //  keyExtractor={(item) => item.playerId.toString()}
+                      keyExtractor={(item, index) => index.toString()}
+                  />
+              </View>
+              <View style={styles.playerContentBox}>
+                  <FlatList
+                      data={teamB}
+                      renderItem={renderTeamBItem}
+                    //  keyExtractor={(item) => item.playerId.toString()}
+                      keyExtractor={(item, index) => index.toString()} //updated dec-3
+                  />
+              </View>
+          </View>
+          <Text style={[styles.headingText, styles.ennum,{textAlign : 'center'}]}>Bench</Text>
+          <View style={styles.playerContainer}>
+              <View style={styles.playerContentBox}>
+                  <FlatList
+                      data={teamABench}
+                      renderItem={renderTeamAItem}
+                      keyExtractor={(item, index) => index.toString()}
+                      ListFooterComponent={<View style={{ paddingBottom: 50 }} />}
+                  />
+              </View>
+              <View style={styles.playerContentBox}>
+                  <FlatList
+                      data={teamBBench}
+                      renderItem={renderTeamBItem}
+                      keyExtractor={(item, index) => index.toString()}
+                      ListFooterComponent={<View style={{ paddingBottom: 50 }} />}
+                  />
+              </View>
+          </View>
+          </ScrollView>
+          <View style={{width: "100%", backgroundColor: graybg, borderRadius: 14, paddingHorizontal: 6, paddingVertical: 5,marginLeft: 'auto',marginRight: 'auto', borderTopColor: "red"}}>
+              <View style={styles.crtimg}>
+                  <ActivityIndicator size="large" color="#279bff" />
+              </View>
+              <Text style={styles.sucstxt}>Waiting for opponent's pick</Text>
+            </View>
+        </View>
+        
+
+
+
+      </Modal>
+  );
+};
+
+export default LoadingPopup;
+
+
+
